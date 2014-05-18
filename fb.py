03bbc74a1ee247d85c98f6c32c346fb3c9ba09b8
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2014-05-18 19:43:01 -0400

    Deleting process_like

--- fb.py
+++ fb.py
@@ -4,7 +4,7 @@ import urllib2
 
 # QUERY
 # Access token
-token = 'CAACEdEose0cBAAroE8TmmEIZB1PJZBchts1SLdgrVfWTIFfDjWr37q7htQLsuRXFAiDe1SqFzAkKNscQQEc53jpizC0L8NxZCXCXWtQlyjwfcJubcr9xuYDFqmTxosXjtzFfaYtSPpb4WEqaMrq1OTeZAD1ajRZCv0w3aluZCGd2a4NX6TP0N8etvkluBAINI9sGZB9l0EPZAQZDZD'
+token = 'CAACEdEose0cBAHqbPZBjrDk1JqPgcTYozRIZBzfv9JPDkHLxQBBnvuGaL3ms06jY1lMN9oVaJeJZBE4J8ixOfSx4O1ZBdpCZATIcrlNHMMPAd4SyZBHkLfNRgH031pSxjVT08eaLdDe2OoFrzu42JZBfEzuVRZBUSnSfYK45JzdJy33X8OJiYZAZAAZCYbzqUcZBxYliYply0t2GRwZDZD'
 
 # Api
 api = 'https://graph.facebook.com'
@@ -34,47 +34,53 @@ COMMENTS = 3
 
 
 # FUNCTIONS
-def post_likes(post_id):
-    action = '/tasteofhome/' + post_id + '/likes'
-    url = q(action)
-    likes_network(url)
+# Graph functions
+def process_shares(shares, **extra):
+    print shares
+    print 'sddasdasd'
+    post_id = extra['post']
+    post_user_id = extra['user']
+    for share in shares:
+        print share
 
-def post_shares(post_id):
-    action = '/tasteofhome/' + post_id + '/sharedposts'
-    url = q(action)
-    shares_network(url)
-
-def post_comments(post_id):
-    action = '/tasteofhome/' + post_id + '/comments'
-    url = q(action)
-    comments_network(url)
+def process_likes(likes, **extra):
+    post_id = extra['post']
+    post_user_id = extra['user']
+    for like in likes:
+        user_id = like['id']
+        if not toh.has_node(user_id):
+            user_info = {
+                'type': 'user',
+                'name': like['name']
+            }
+            toh.add_node(user_id, user_info)
+            toh.add_edge(user_id, toh_id, label='is_member_of')
+        toh.add_edge(user_id, post_id, label='likes')
+        toh.add_edge(user_id, post_user_id, label='is_friend_of')
 
 def process_posts(posts):
     for post in posts:
-        # author info
-        author_id = post['from']['id']
-        if author_id != toh_id:
-            author_info = {
+        # user info
+        user_id = post['from']['id']
+        if not toh.has_node(user_id):
+            user_info = {
                 'type': 'user',
                 'name': post['from']['name']
             }
-            if not toh.has_node(author_id):
-                toh.add_node(author_id, author_info)
-                toh.add_edge(author_id, toh_id, label='likes')
+            toh.add_node(user_id, user_info)
+            toh.add_edge(user_id, toh_id, label='is_member_of')
         # post info
-        ide = post['id']
-        message = post.get('message', '')
-        info = {
+        post_id = post['id']
+        post_info = {
             'type': 'post',
-            'message': message
+            'message': post.get('message', '')
         }
-        if not toh.has_node(ide):
-            toh.add_node(ide, info)
-            toh.add_edge(author_id, ide, label='posts')
-        # post actions
-        post_likes(ide)
-        post_shares(ide)
-        post_comments(ide)
+        toh.add_node(post_id, post_info)
+        toh.add_edge(user_id, post_id, label='posts')
+        # post actions: likes, shares, comments
+        #post_likes(post_id, user_id)
+        post_shares(post_id, user_id)
+#        comment_users = post_comments(post_id, user_id)
     
     global_var['post_counter'] += len(posts)
     if global_var['post_counter'] % 100 == 0:
@@ -82,18 +88,19 @@ def process_posts(posts):
     if global_var['post_counter'] % 1000 == 0:
         nx.write_gexf(toh, 'toh.gexf')
 
-def process_data(data, result_type):
-    if data['data']:
+# Data processing functions
+def process_data(data, result_type, **extra):
+    if 'data' in data:
         if result_type == POSTS:
             process_posts(data['data'])
         elif result_type == LIKES:
-            process_likes(data['data'])
+            process_likes(data['data'], **extra)
         elif result_type == SHARES:
-            process_shares(data['data'])
+            process_shares(data['data'], **extra)
         else: # result_type == COMMENTS:
-            process_comments(data['data'])
-    if data['paging'] and data['paging']['next']:
-        process_query(data['paging']['next'], result_type)
+            process_comments(data['data'], **extra)
+    if 'paging' in data and 'next' in data['paging']:
+        process_query(data['paging']['next'], result_type, **extra)
 
 def get_data(url):
     response = urllib2.urlopen(url)
@@ -102,45 +109,50 @@ def get_data(url):
     data = json.loads(content)
     return data
 
-def process_query(url, result_type):
+def process_query(url, result_type, **extra):
     data = get_data(url)
-    process_data(data, result_type)
+    process_data(data, result_type, **extra)
 
-def comments_network(url)
-    process_query(url, COMMENTS)
+# Main functions
+def post_comments(post_id, user_id):
+    action = '/' + post_id + '/comments'
+    url = q(action)
+    process_query(url, COMMENTS, post=post_id, user=user_id)
 
-def shares_network(url)
-    process_query(url, SHARES)
+def post_shares(post_id, user_id):
+    action = '/' + post_id + '/sharedposts'
+    url = q(action)
+    process_query(url, SHARES, post=post_id, user=user_id)
 
-def likes_network(url)
-    process_query(url, LIKES)
+def post_likes(post_id, user_id):
+    print 'post_likes'
+    action = '/' + post_id + '/likes'
+    url = q(action)
+    process_query(url, LIKES, post=post_id, user=user_id)
 
-def toh_network(url)
+def toh_posts():
+    action = '/tasteofhome/feed'
+    url = q(action)
     process_query(url, POSTS)
 
 
 # NETWORK
-#Graph
+# Graph
 toh = nx.Graph()
 
-# Central node
+# Add toh node
 action = '/tasteofhome?fields=name'
 url = q(action)
 data = get_data(url)
-ide = data['id']
-info = {
+toh_id = data['id']
+toh_info = {
     'type': 'toh',
     'name': data['name'],
-    'location': data['location']['city']
 }
-toh.add_node(ide, info)
-toh_id = ide
+toh.add_node(toh_id, toh_info)
 
-# Network
-action = '/tasteofhome/feed'
-url = q(action)
-toh_network(url)
-
+# Process toh relations
+toh_posts()
 
 
 
