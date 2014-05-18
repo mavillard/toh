c63484f8f1e1e8a3b2db3ce2d689e8762af083e0
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2014-05-18 00:15:49 -0400

    Users and posts

--- fb.py
+++ fb.py
@@ -2,8 +2,9 @@ import json
 import networkx as nx
 import urllib2
 
+# Query
 # Access token
-token = 'CAACEdEose0cBAKvE4ANEG6InnkhpZAaiPzWWjZBHAUse2ytZAJaXQbbHnKn3UUNLJevEKx8uFT5BmZB4dDAuu8KWA0xd1Gq48ciwFIg10BfvbPq32G5Uw4ZCeUcUz6JQ3u9JVq9puZAhQKKeEjtE51FONEJCNgrlmV4NmAG6FgtPRj2ce1WUuY06UHtRC0MLy1OUCG41BzCQZDZD'
+token = 'CAACEdEose0cBAOKC0sZBBxxJviYSVTvzz6qVQrPojOzuY3ZCtlFZBLzyZCyvlOJBWMZCO4L8ZCUd30mbVKbej6AZASQAGMeT0YBtcbDyAKEdXe4bfzQFoPdmVdzFWNASrBCrb3E8brLarZAUzJ9x3fY1Csyurpd7YfUAZBqkJ29BjGXSbg3kaDhDZCas5QXHrJYaVKJOiNVZApyLgZDZD'
 
 # API
 api = 'https://graph.facebook.com'
@@ -23,28 +24,74 @@ def q(action):
     return query
 
 
-# Network
-def toh_network():
-    action = '/tasteofhome/feed'
-    url = q(action)
+# FUNCTIONS
+# Global var
+global_var = {'posts': 0}
+
+def process_posts(posts):
+    for post in posts:
+        # post info
+        ide = post['id']
+        message = post.get('message', '')
+        info = {
+            'type': 'post',
+            'message': message
+        }
+        toh.add_node(ide, info)
+        # author info
+        author_id = post['from']['id']
+        if author_id != toh_id:
+            author_info = {
+                'type': 'user',
+                'name': post['from']['name']
+            }
+            toh.add_node(author_id, author_info)
+            toh.add_edge(author_id, ide, label='posts')
+            toh.add_edge(author_id, toh_id, label='likes')
+    global_var['posts'] += len(posts)
+    print global_var['posts'], 'posts processed...'
+
+def process_data(data):
+    if data['data']:
+        process_posts(data['data'])
+    if data['paging'] and data['paging']['next']:
+        toh_network(data['paging']['next'])
+
+def toh_network(url):
+    data = get_data(url)
+    process_data(data)
+
+# Get data from url
+def get_data(url):
     response = urllib2.urlopen(url)
     content = response.read()
     response.close()
     data = json.loads(content)
-    process_data(data)
+    return data
 
-# Process data
-def process_data(data):
-    if data['data']:
-        process_posts(data['data'])
-    if data['paging']:
-        pass #while
 
-# Process posts
-def process_posts(posts):
-    for post in posts:
-        print 'author:', post['from']['name']
-        print '---'
+# NETWORK
+toh = nx.Graph()
+
+# Central node
+action = '/tasteofhome?fields=name'
+url = q(action)
+data = get_data(url)
+ide = data['id']
+info = {
+    'type': 'toh',
+    'name': data['name'],
+}
+toh.add_node(ide, info)
+toh_id = ide
+
+# Network from posts
+action = '/tasteofhome/feed'
+url = q(action)
+toh_network(url)
+
+
+
 
 
 
